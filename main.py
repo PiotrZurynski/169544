fc78c3bcef6754bfc72efdbfe2100bad7a41b6e0
PiotrZurynski <pewdiejanusz@gmail.com>
2023-03-08 11:02:09 +0100

    zmiana

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ import sys
 print(sys.version)
 
 print(5)
+print("nic")
+fruits =["apple","bananas","grapes"]
+for x in fruits:
+    print(x)
+
+
+i=1
+while i<=6:
+    print(i)
+    i+=1
+
+a="""nicnicninicnicninciicniadkksadlsakdlsa dsladlsaldksa dsadlksaldksakdlskaldkasld"""
+print(a)
+print(len(a))
+
+
+
+
