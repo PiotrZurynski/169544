25640dd3c989c1d300a557cab7d207da7ddd9c42
PiotrZurynski <115088277+PiotrZurynski@users.noreply.github.com>
2023-05-24 12:48:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,138 +1,175 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+import numpy as np
+import seaborn as sns
 from PIL import Image
-import math
-import random
 
-#plt.plot([1,3,5,7])
-#plt.show()
+#4 zadania
+# same csv (brak excela)
+# zapisywanie do pliku
+#na kolosie nie ma losowych danych
+#wykresy oraz kod wyslane msteams
+#wykresy jako obrazy wyslane msteams
+#Zadanie 1
+#matplotlib jakas funkcja wykres liniowy (jakas funkcja w zadaniu i na wykresie mabyc
+#zad 2
+#siatka wykresy lewo prawo gora dol
+#zad 3
+#ramka danych grupa wykres zrobienie wykresu
+#zad 4 wykres z matplotliba lub z seaborna
 
-#x=np.array([1,2,3,4])
-#y=x**2
-#plt.plot(x,y,'ko-')
-#plt.axis([0,6,0,20])
-#plt.show()
+#Random Chart with Moving Average
+# ts=pd.Series(np.random.randn(1000))
+# ts=ts.cumsum()
+#
+# df=pd.DataFrame(ts,columns=['wartosci'])
+# print(df)
+# df['Srednia kroczaca']=df.rolling(window=50).mean()
+# df.plot()
+# plt.legend()
+# plt.show()
+#Histogram z siatka z kolorkiem z przezroczystoscia 0.5 superanckohaj
+# x=np.random.randn(10000)
+# plt.hist(x,bins=50,facecolor='y',alpha=0.5,density=True)
+# plt.xlabel('Wartości')
+# plt.ylabel('Prawdopodobieństwo')
+# plt.title('Histogram')
+# plt.grid()
+# plt.show()
+#Wykres kolowy matplotlib
+# df=pd.read_csv('dane.csv',header=0,sep=";",decimal=".")
+# print(df)
+# seria=df.groupby('Imię i nazwisko')['Wartość zamówienia'].sum()
+# wedges, texts, autotext =plt.pie(x=seria,labels=seria.index,
+#                                  autopct=lambda pct: "{:.1f}%".format(pct),
+#                                  textprops=dict(color="magenta"),
+#                                  colors=['blue','cyan'])
+# plt.title('Suma zamówień dla sprzedawcow')
+# plt.legend(loc='lower right')
+# plt.ylabel('Procentowy wynik wartości zamówienia')
+# plt.show()
+#Wykres seaborn!
+# sns.set(rc={'figure.figsize': (5, 5)})
+# sns.lineplot(x=[1, 2, 3, 4], y=[1, 4, 9, 16],label='linia nr1', color='red', marker='o', linestyle=':')
+# sns.lineplot(x=[1, 2, 3, 4], y=[2, 5, 10, 17],label='linia nr2', color='green', marker='^', linestyle=':')
+# plt.xlabel('oś x')
+# plt.ylabel('oś y')
+# plt.title('Wykres liniowy')
+# plt.show()
+#Wykres seaborn pt2
+# s=pd.Series(np.random.randn(1000))
+# s=s.cumsum()
+# sns.set()
+#
+# wykres = sns.relplot(kind='line', data=s, label='linia')
+# wykres.fig.set_size_inches(7, 6)
+# wykres.fig.suptitle('Wykres liniowy losowych danych')
+# wykres.set_xlabels('indeksy')
+# wykres.set_ylabels('wartości')
+# wykres.add_legend()
+# wykres.figure.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
+# plt.show()
 
-#plt.plot(x,y,'r')
-#plt.plot(x,y,'o')
-#plt.axis([0,6,0,20])
-#plt.show()
+#Wykres kolumnowy
+# data = {'Kraj': ['Belgia', 'Indie', 'Brazylia', 'Polska'],'Stolica': ['Bruksela', 'New Delhi', 'Brasilia', 'Warszawa'],'Kontynent': ['Europa', 'Azja', 'AmerykaPołudniowa', 'Europa'],'Populacja': [11190846, 1303171035, 207847528, 38675467]}
+# df = pd.DataFrame(data)
+# sns.set()
+# plot = sns.catplot(data=df, x='Kontynent', y='Populacja', kind='bar', ci=None, hue='Kontynent', estimator=np.sum,dodge=False, palette=['red', 'green', 'yellow'], legend_out=False)
+# plot.fig.set_size_inches(7, 6)
+# plot.add_legend(title='Populacja na kontynentach', loc='upper right')
+# plot.fig.suptitle('Populacja na kontynentach')
+# plot = sns.barplot(data=df, x='Kontynent', y='Populacja', ci=None, hue='Kontynent', estimator=np.sum,dodge=False, palette=['red', 'green', 'yellow'])
+# plot.legend(title='Populacja na kontynentach')
+# plot.set(title='Wykres słupkowy')
+# plt.show()
 
+#Wykres liniowy z wykorzystaniem ramki danych
+# sns.set()
+# df = pd.read_csv('iris.data', header=0, sep=',', decimal='.')
+# print(df)
+# wykres=sns.lineplot(data=df,x=df.index,y='sepal length',hue='class',palette=['red','green','blue'])
+# wykres.set_xlabel('indeksy')
+# wykres.set_title('Wykres liniowy dancyh z Iris dataset')
+# wykres.legend(title='Rodzaj kwiatu')
+# plt.show()
 
-#x=np.arange(0,5,0.2)
-#plt.plot(x,x,'r--',x,x**2,'bo',x,x**3,'g^',)
-#plt.legend(labels=['liniowa','kwadratowa','sześcienna'])
-#plt.show()
+# #Wykres punktowy
+# sns.set()
+# data={'a':np.arange(10),'c':np.random.randint(0,50,10),'d':np.random.randn(10)}
+# data['b']=data['a']+10*np.random.randn(10)
+# data['d']=np.abs(data['d'])*100
+# print(data['c'])
+# print(data['d'])
+# df=pd.DataFrame(data)
+# plot=sns.relplot(data=df,x="a",y="b",hue="c",palette='bright',size="d",legend=True)
+# plot.fig.set_size_inches(6,6)
+# plot.set(xticks=data['a'])
+# plt.show()
 
-#x=np.arange(0,5,0.2)
-#plt.plot(x,x,'r--',label='liniowa')
-#plt.plot(x,x**2,'bo',label='kwadratowa')
-#plt.plot(x,x**3,'g^',label='sześcienna')
-#plt.legend(loc='best')
-#plt.show()
+#Wykres kolumnowy
+# data = {'Kraj': ['Belgia', 'Indie', 'Brazylia', 'Polska'],'Stolica': ['Bruksela', 'New Delhi', 'Brasilia', 'Warszawa'],'Kontynent': ['Europa', 'Azja', 'AmerykaPołudniowa', 'Europa'],'Populacja': [11190846, 1303171035, 207847528, 38675467]}
+# df = pd.DataFrame(data)
+# sns.set()
+# # plot = sns.catplot(data=df, x='Kontynent', y='Populacja', kind='bar', ci=None, hue='Kontynent', estimator=np.sum,dodge=False, palette=['red', 'green', 'yellow'], legend_out=False)
+# # plot.fig.set_size_inches(7, 6)
+# # plot.fig.suptitle('Populacja na kontynentach')
+# #plot.add_legend(title='Populacja na kontynentach')
+# plot = sns.barplot(data=df, x='Kontynent', y='Populacja', ci=None, hue='Kontynent', estimator=np.sum,dodge=False, palette=['red', 'green', 'yellow'])
+# plot.legend(title='Populacja na kontynentach')
+# plot.set(title='Wykres słupkowy')
+# plt.show()
 
+#Wykres liniowy pandas
+# ts=pd.Series(np.random.randn(1000))
+# ts=ts.cumsum()
+# print(ts)
+# ts.plot()
+# plt.show()
 
-#x=np.arange(0,10.1,0.1)
-#plt.plot(x,np.tan(x),'r-',label='sinus')
-#plt.xlabel('wartosci x')
-#plt.ylabel('wartosci y')
-#plt.legend()
-#plt.title('wykres liniowy funkcji sin(x)')
-#plt.show()
+#Wykres kolumnowy z Pandas DataFrame
+# data={'Kraj':['Belgia','Indie','Brazylia','Polska'],
+#       'Stolica':['Bruksela','New Delhi','Brasilia','Warszawa'],
+#       'Kontynent':['Europa','Azja','Ameryka Południowa','Europa'],
+#       'Populacja':[11190846,1303171035,207847528,38675467]}
+# df=pd.DataFrame(data)
+# print(df)
+# grupa=df.groupby(['Kontynent']).agg({'Populacja':['sum']})
+# print(grupa)
+# wykres=grupa.plot.bar()
+# wykres.set_ylabel("M1d")
+# wykres.set_xlabel('Kontynent')
+# wykres.tick_params(axis='x',labelrotation=0)
+# wykres.legend()
+# wykres.set_title('Populacja z podziałem na kontynenty')
+# plt.savefig('wykres.png')
+# plt.show()
 
-#data={'a':np.arange(50),'c':np.random.randint(0,50,50),'d':np.random.randn(50)}
-
-#data['b']=data['a']+10*np.random.randn(50)
-#data['d']=np.abs(data['d'])*100
-#plt.scatter('a','b',c='c',s='d',data=data,cmap='plasma')
-#plt.xlabel('klucz a')
-#plt.ylabel('klucz b')
-#plt.show()
-
-#x1=np.arange(0,2,0.02)
-#x2=np.arange(0,2,0.02)
-
-#y1=np.sin(x1*np.pi*2)
-#y2=np.cos(x2*np.pi*2)
-
-#plt.subplot(4,1,1)
-#plt.plot(x1,y1)
-#plt.title('Wykres sin(x1)')
-#plt.xlabel('x')
-#plt.ylabel('sin(x1)')
-#plt.subplot(4,1,4)
-#plt.plot(x2,y2,'r')
-#plt.title('Wykres cos(x2)')
-#plt.xlabel('x')
-#plt.ylabel('cos(x2)')
-#plt.subplots_adjust(hspace=0.5)
-#plt.show()
-
-#x1=np.arange(0,2,0.02)
-#x2=np.arange(0,2,0.02)
-
-#y1=np.sin(x1*np.pi*2)
-#y2=np.cos(x2*np.pi*2)
-
-#fig,axs=plt.subplots(3,2)
-#axs[0,0].plot(x1,y1)
-#axs[0,0].set_title('Wykres funkcji sinus')
-#axs[0,0].set_xlabel('x')
-#axs[0,0].set_ylabel('y')
-
-#axs[1,1].plot(x2,y2,'k^')
-#axs[1,1].set_title('Wykres funkcji cosinusa')
-#axs[1,1].set_xlabel('x')
-#axs[1,1].set_ylabel('y')
-
-#axs[2,0].plot(x1,y1,'r--')
-#axs[2,0].set_title('Wykres funkcji sinus')
-#axs[2,0].set_xlabel('x')
-#axs[2,0].set_ylabel('y')
-#plt.subplots_adjust(hspace=0.7)
-#fig.delaxes(axs[0,1])
-#fig.delaxes(axs[1,0])
-#fig.delaxes(axs[2,1])
-#plt.show()
-
-data={'Kraj':['Belgia','Indie','Brazylia','Polska'],'Stolica':['Bruksela','New Delhi','Brasilia','Warszawa'],'Kontynent':['Europa','Azja','Ameryka Południowa','Europa'],'Populacja':[11190846,1303171035,207847528,38675467]}
-#df=pd.DataFrame(data)
-#print(df)
-#grupa=df.groupby('Kontynent')
-#etykiety=list(grupa.groups.keys())
-#wartosci=list(grupa.agg('Populacja').sum())
-
-#plt.bar(x=etykiety,height=wartosci,color=['yellow','red','green'])
-#plt.xlabel('Kontynenty')
-#plt.ylabel('Populacja')
-
-#plt.show()
-#grupa=df.groupby('Kontynent').agg({'Populacja':['sum']})
-#print(grupa)
-
-#grupa.plot(kind='bar',xlabel='Kontynenty',ylabel='Populacja',
-           #legend=True,rot=0,title='Populacja na kontynentach')
-#plt.savefig('wykres.png')
-#wykres=grupa.plot.bar()
-#wykres.set_ylabel('Populacja')
-#wykres.set_xlabel('Kontynenty')
-#wykres.tick_params(axis='x',labelrotation=0)
-#wykres.legend()
-#wykres.set_title("Populacja na kontynentach")
-
-#plt.show()
-
-
-#ts=pd.Series(np.random.randn((1000)))
-#ts=ts.cumsum()
-#ts.plot()
-#plt.show()
-
-df=pd.read_csv('dane.csv',header=0,sep=';',decimal='.')
+#czytywanie danych z pliku i wyswietlenie grupowanych wartosci
+# df=pd.read_csv('dane.csv',header=0,sep=";",decimal=".")
+# print(df)
+# grupa=df.groupby(['Imię i nazwisko']).agg({'Wartość zamówienia':['sum']})
+# grupa.plot(kind='pie',subplots=True,autopct='%.2f%%',fontsize=20,figsize=(6,6),colors=['dodgerblue','darkorange'])
+# plt.legend(loc='lower right')
+# plt.title('Suma zamowienia dla sprzedawcy')
+# plt.show()
+#Zadanie 1
+df=pd.read_excel('imiona.xlsx',header=0)
 print(df)
-grupa=df.groupby('Imie i nazwisko').agg({'Wartość zamówienia':['sum']})
-grupa.plot(kind='pie',subplots=True,autopct='%.2f%',fontsize=20,figsize=(6,6),colors=['red','green'])
-plt.legend(loc='lower right')
-plt.title('Procent zamowienia dla sprzedazy')
+
+# grupa=df.groupby(['Rok']).agg({'Liczba':['sum']})
+# grupa.plot()
+# plt.ylabel('Liczba urodzeń')
+# plt.xlabel('Lata')
+# plt.title('Wykres liczby urodzeń')
+# plt.show()
+
+grupa=df.groupby(['Plec']).agg({'Liczba':['sum']})
+wykres=grupa.plot(kind='bar',colors=['green','blue'])
+wykres.set_xlabel('M i K')
+wykres.set_ylabel("M1d")
+wykres.tick_params(axis='x',labelrotation=0)
+wykres.legend()
+wykres.set_title('Ilosc urdzonych chlopow i bab')
+plt.savefig('wykres1.png')
 plt.show()
+
